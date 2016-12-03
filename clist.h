d10370a3ba69058c2baeb342ed1799c14e3d0c10
Billy <wild188@gmail.com>
2016-12-03 12:01:58 -0500

    frist pass at clist implementation

--- clist.h
+++ clist.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <mutex>
 
 /// TODO: complete this implementation of a thread-safe (concurrent) sorted
 /// linked list of integers
 class clist {
     /// a node consists of a value and a pointer to another node
+
+    private std::mutex gate;
+
+
     struct node
     {
         int value;
@@ -16,9 +21,9 @@ class clist {
   public:
     /// insert *key* into the linked list if it doesn't already exist; return
     /// true if the key was added successfully.
-    bool insert(int key) { 
+    bool og_insert(int key) { 
       node *temp;
-      //obtain lock using mutex
+
       if(head){
 	temp = head;
 	if(temp->value == key)return false;
@@ -41,7 +46,6 @@ class clist {
             mynode->next = temp;
             return true
       } 
-	//temp = temp->next; //Nate I think this would skip some
       } 
       
       if(temp->value == key)return false;
@@ -51,15 +55,88 @@ class clist {
       prev->next = mynode;
       mynode->next = temp;
       }
-      //release lock
       return true;
 
     }
+
+    bool insert(int key){
+          gate.lock();
+          bool result = og_insert(key);
+          gate.unlock();
+          return result;
+    }
+
+    bool og_remove(int key){
+      node *temp;
+
+      if(head){
+	temp = head;
+	if(temp->value == key){
+            head = head->next;
+            delete(temp);
+            return true;
+      }
+      }else{
+            return false;
+      }
+
+      while(temp->next){
+	node * prev = temp;
+	temp = temp->next;
+	if(temp->value == key){
+            prev->next = temp->next;
+            delete(temp);
+            return true;
+      }
+	else if(temp->value > key){
+            return false
+      } 
+      }
+
+      return false;
+    }
     /// remove *key* from the list if it was present; return true if the key
     /// was removed successfully.
-    bool remove(int key) { return false; }
+    bool remove(int key) {
+          gate.lock();
+          bool result = og_remove(key);
+          gate.unlock();
+          return result;
+    }
+
+    bool og_lookup(int key){
+          node *temp;
+
+      if(head){
+	temp = head;
+	if(temp->value == key)return true;
+      }else{
+            return false;
+      }
+
+      while(temp->next){
+	node * prev = temp;
+	temp = temp->next;
+	if(temp->value == key){
+            return true;
+      }
+	else if(temp->value > key){
+            return false
+      } 
+      }
+
+      return false;
+    }
+
     /// return true if *key* is present in the list, false otherwise
-    bool lookup(int key) { return false; }
+    bool lookup(int key) {
+          gate.lock();
+          bool result = og_lookup();
+          gate.unlock();
+          return result;
+    }
     /// constructor code goes here
-    clist(int): head(NULL) { }
+    clist(int): head(NULL) {
+
+     }
 };
