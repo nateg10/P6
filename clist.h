a5930835bd78134d45c9c8f8c925ddbfe911b9ac
Nathaniel Gyory <npg216@dactyl.cse.lehigh.edu>
2016-12-02 19:34:22 -0500

    wrote insert

--- clist.h
+++ clist.h
@@ -16,7 +16,34 @@ class clist {
   public:
     /// insert *key* into the linked list if it doesn't already exist; return
     /// true if the key was added successfully.
-    bool insert(int key) { return false; }
+    bool insert(int key) { 
+      node *temp;
+      //obtain lock using mutex
+      if(head){
+	temp = head;
+	if(temp->value == key)return false;
+      } 
+      else{
+	head = new node();
+	head->value = key;
+	return true;
+      }
+
+      while(temp->next){
+	temp = temp->next;
+	if(temp->value == key) return false;
+	temp = temp->next;
+      } 
+      
+      if(temp->value == key)return false;
+      else{
+	temp->next = new node();
+	temp->next->value = key;
+      }
+      //release lock
+      return true;
+
+    }
     /// remove *key* from the list if it was present; return true if the key
     /// was removed successfully.
     bool remove(int key) { return false; }
