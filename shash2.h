92842be31c9f8f53cc1720a64e6be4984bd7c7c8
Nathaniel Gyory <npg216@eris.cse.lehigh.edu>
2016-12-06 15:18:34 -0500

    halfway through t5

--- shash2.h
+++ shash2.h
@@ -1,3 +1,4 @@
+#include "clist"
 #pragma once
 
 /// TODO: complete this implementation of a thread-safe (concurrent) hash
@@ -7,15 +8,68 @@
 ///       In addition, the API now allows for multiple keys on each
 ///       operation.
 class shash2 {
+  
+  node **buckets;
+  
+ private:
+
+  struct node{
+    int value;
+    node* next 
+    std::mytex gate;
+  };
+  
+  int len;
+
   public:
+
+  bool og_insert(int key, node* head){
+  }
+
+  bool og_remove(int key, node* head){
+  }
+
+  bool og_lookup(int key, node* head){
+  }
+
+
+  int getHash(int key){
+    return key % len;
+  }
     /// insert /num/ values from /keys/ array into the hash, and return the
     /// success/failure of each insert in /results/ array.
-    void insert(int* keys, bool* results, int num) { }
+    void insert(int* keys, bool* results, int num) {
+      for(int x = 0; x < num; x++){
+	int index = getHash(keys[x]);
+	results[x] = og_insert(keys[x], buckets[index]);
+	return res;
+      }
+    }
     /// remove *key* from the list if it was present; return true if the key
     /// was removed successfully.
-    void remove(int* keys, bool* results, int num) { }
+    void remove(int* keys, bool* results, int num) { 
+      for(int x = 0; x < num; x++){
+        int index = getHash(keys[x]);
+	results[x] = og_remove(keys[x], buckets[index]);
+	return res;
+      }
+    }
     /// return true if *key* is present in the list, false otherwise
-    void lookup(int* keys, bool* results, int num) { }
+    void lookup(int* keys, bool* results, int num) { 
+      for(int x = 0; x < num; x++){
+        int index = getHash(keys[x]);
+	results[x] = og_insert(keys[x], buckets[index]);
+	return res;
+      }
+    }
     /// constructor code goes here
-    shash2(unsigned _buckets) { }
+ shash2(unsigned _buckets):len(_buckets) { 
+      int x = 0;
+      buckets = (node**)malloc(sizeof(node) * len);
+      while(x < len){
+	node *head = new node(len);
+	buckets[x] = member;
+	x++;
+      }
+    }
 };
